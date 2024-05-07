15a2008165151b31d3352a183b5d9ea89be0e750
thee-falcon <omar.makran.10@gmail.com>
2024-05-07 17:10:43 +0100

    min and max function templates is done

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: omakran <omakran@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 16:03:51 by omakran           #+#    #+#             */
-/*   Updated: 2024/05/07 16:24:33 by omakran          ###   ########.fr       */
+/*   Updated: 2024/05/07 17:08:37 by omakran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ int a = 2;
 int b = 3;
 ::swap( a, b );
 std::cout << "a = " << a << ", b = " << b << std::endl;
-// std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-// std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-// std::string c = "chaine1";
-// std::string d = "chaine2";
-// ::swap(c, d);
-// std::cout << "c = " << c << ", d = " << d << std::endl;
-// std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-// std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+std::string c = "chaine1";
+std::string d = "chaine2";
+::swap(c, d);
+std::cout << "c = " << c << ", d = " << d << std::endl;
+std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 return 0;
 }
--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: omakran <omakran@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 16:04:04 by omakran           #+#    #+#             */
-/*   Updated: 2024/05/07 16:26:49 by omakran          ###   ########.fr       */
+/*   Updated: 2024/05/07 17:06:48 by omakran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,26 @@
 # include <iostream>
 
 template <typename T>
-
 void    swap(T &a, T &b) {
     T   tmp;
 
     tmp = a;
     a = b;
     b = tmp;
+}
+
+/*
+the intention is to only compare the values and not modify them,
+it's better to use 'const' to indicate that intention and to prevent accidental modification of the arguments.
+*/
+template <typename T>
+T min(const T& a, const T& b) {
+    return (b < a) ? b : a;
+}
+
+template <typename T>
+T max(const T& a, const T& b) {
+    return (a < b) ? b : a;
 }
 
 # endif
